86a793e1ddd13c87dd0d2615d654293c0f1ce628
nilscox <nils@koyeb.com>
2024-02-14 14:44:38 +0100

    nodejs http server

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from "node:http";
+
+const port = Number(process.env.PORT);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("okay");
+});
+
+server.listen(port, "127.0.0.1", () => {
+  console.log(`Server listening on port ${port}`);
+});
